aaa654894cc82bc477418df586cf77b248ff48e0
Ankur Seth <iankurseth@gmail.com>
2023-05-10 11:24:30 +0530

    Added tests for isNumeric, published jslice@0.7.0

--- tests/isNumeric.test.ts
+++ tests/isNumeric.test.ts
@@ -2,4 +2,21 @@ const { isNumeric } = require("../dist/utils");
 
 test("isNumeric", () => {
 	expect(isNumeric("0")).toStrictEqual(true);
+	expect(isNumeric("20")).toStrictEqual(true);
+	expect(isNumeric("0.5")).toStrictEqual(true);
+	expect(isNumeric("5.60")).toStrictEqual(true);
+	expect(isNumeric("-20.5")).toStrictEqual(true);
+
+	expect(isNumeric("0 0")).toStrictEqual(false);
+	expect(isNumeric("2 0")).toStrictEqual(false);
+	expect(isNumeric("0. 5")).toStrictEqual(false);
+	expect(isNumeric("5.6 0")).toStrictEqual(false);
+	expect(isNumeric("-2 0.5")).toStrictEqual(false);
+
+	expect(isNumeric()).toStrictEqual(false);
+	expect(isNumeric(false)).toStrictEqual(false);
+	expect(isNumeric(null)).toStrictEqual(false);
+	expect(isNumeric(20)).toStrictEqual(false);
+	expect(isNumeric(1.50)).toStrictEqual(false);
+	expect(isNumeric(4/5)).toStrictEqual(false);
 });
